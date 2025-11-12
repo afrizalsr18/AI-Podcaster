144b19a8749a58a83623d78b1845fb81d1e68d78
Afrizal S <afrizalsrizki@gmail.com>
2025-11-12 15:18:34 +0700

    feat(rightSidebar): user detail, similar podcast, detail player

--- app/(root)/podcasts/[podcastId]/page.tsx
+++ app/(root)/podcasts/[podcastId]/page.tsx
@@ -1,83 +1,86 @@
 'use client'
 
-import React from 'react'
+import EmptyState from '@/components/EmptyState'
+import LoaderSpinner from '@/components/ui/LoaderSpinner'
+import PodcastCard from '@/components/PodcastCard'
+import PodcastDetailPlayer from '@/components/PodcastDetailPlayer'
+import { api } from '@/convex/_generated/api'
+import { Id } from '@/convex/_generated/dataModel'
+import { useUser } from '@clerk/nextjs'
+import { useQuery } from 'convex/react'
 import Image from 'next/image'
-import {api} from '@/convex/_generated/api'
-import {useQuery} from "convex/react";
-import {Id} from  '@/convex/_generated/dataModel'
-import PodcastDetailPlayer from "@/components/PodcastDetailPlayer";
-import LoaderSpinner from "@/components/ui/LoaderSpinner";
-import PodcastCard from "@/components/PodcastCard";
+import React from 'react'
 
-const PodcastDetails = ({ params : {podcastId} }: {
-  params: {podcastId: Id<'podcasts'>} }) => {
-  const podcast = useQuery(api.podcasts.getPodcastById, {podcastId})
+const PodcastDetails = ({ params: { podcastId } }: { params: { podcastId: Id<'podcasts'> } }) => {
+  const { user } = useUser();
+  const podcast = useQuery(api.podcasts.getPodcastById, { podcastId })
 
-    const similarPodcasts = useQuery(api.podcasts.getPodcastByVoiceType, {podcastId})
-    if(!similarPodcasts || !podcast) return <LoaderSpinner />
+  const similarPodcasts = useQuery(api.podcasts.getPodcastByVoiceType, { podcastId })
+
+  const isOwner = user?.id === podcast?.authorId;
+
+  if (!similarPodcasts || !podcast) return <LoaderSpinner />
 
   return (
     <section className="flex w-full flex-col">
-      <header className="mt-6 flex item-center justify-between">
+      <header className="mt-9 flex items-center justify-between">
         <h1 className="text-20 font-bold text-white-1">
-          Currently Playing
+          Currenty Playing
         </h1>
         <figure className="flex gap-3">
           <Image
             src="/icons/headphone.svg"
-            width={20}
-            height={20}
+            width={24}
+            height={24}
             alt="headphone"
           />
-          <h2 className="text-16 font-bold text-white-1">
-            {podcast?.views}
-          </h2>
+          <h2 className="text-16 font-bold text-white-1">{podcast?.views}</h2>
         </figure>
       </header>
 
-      <PodcastDetailPlayer />
+      <PodcastDetailPlayer
+        isOwner={isOwner}
+        podcastId={podcast._id}
+        {...podcast}
+      />
 
-      <p className='text-white-2 text-16 pb-8 pt-[45px] font-medium max-md:text-center'> {podcast?.podcastDescription}</p>
+      <p className="text-white-2 text-16 pb-8 pt-[45px] font-medium max-md:text-center">{podcast?.podcastDescription}</p>
 
       <div className="flex flex-col gap-8">
-        <div className="flex flex-col gap-4">
-          <h1 className="text-18 font-bold text-white-2">
-            Transcription</h1>
+        <div className='flex flex-col gap-4'>
+          <h1 className='text-18 font-bold text-white-1'>Transcription</h1>
           <p className="text-16 font-medium text-white-2">{podcast?.voicePrompt}</p>
         </div>
-        <div className="flex flex-col gap-4">
-          <h1 className="text-18 font-bold text-white-2">
-            Thumbnail Prompt</h1>
+        <div className='flex flex-col gap-4'>
+          <h1 className='text-18 font-bold text-white-1'>Thumbnail Prompt</h1>
           <p className="text-16 font-medium text-white-2">{podcast?.imagePrompt}</p>
         </div>
       </div>
-
       <section className="mt-8 flex flex-col gap-5">
         <h1 className="text-20 font-bold text-white-1">Similar Podcasts</h1>
 
         {similarPodcasts && similarPodcasts.length > 0 ? (
-            <div className='podcast_grid'>
-          {similarPodcasts?.map(({
-            _id,
-            podcastTitle,
-            podcastDescription,
-            imageUrl,
-          }) => (
-            <PodcastCard
-              key={_id}
-              imgUrl={imageUrl}
-              title={podcastTitle}
-              description={podcastDescription}
-              podcastId={_id}
-            />
-          ))}
-        </div>
+          <div className="podcast_grid">
+            {similarPodcasts?.map(({ _id, podcastTitle, podcastDescription, imageUrl }) => (
+              <PodcastCard
+                key={_id}
+                imgUrl={imageUrl as string}
+                title={podcastTitle}
+                description={podcastDescription}
+                podcastId={_id}
+              />
+            ))}
+          </div>
         ) : (
-          <>Empty</>
+          <>
+            <EmptyState
+              title="No similar podcasts found"
+              buttonLink="/discover"
+              buttonText="Discover more podcasts"
+            />
+          </>
         )}
-
       </section>
-
 
     </section>
   )
--- next.config.ts
+++ next.config.ts
@@ -14,6 +14,10 @@ const nextConfig: NextConfig = {
       {
         protocol: 'https',
         hostname: 'lovely-flamingo-139.convex.cloud'
+      },
+      {
+        protocol: 'https',
+        hostname: 'img.clerk.com'
       }
     ]
   },
